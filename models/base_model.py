63304757af78a2d20f6e8a148605ea1913c39658
Sebastian Lopez <lopez.sebastian@correounivalle.edu.co>
2020-02-13 20:26:55 +0000

    Permission of execution and started base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{:s}] ({:s}) {:s}".format(
+            self.__class__.__name__,
+            self.id,
+            str(self.__dict__)
+        )
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+
+        rep = {
+            "__class__": self.__class__.__name__,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
+
+        dict_copy = self.__dict__.copy()
+        dict_copy.update(rep)
+        return dict_copy
