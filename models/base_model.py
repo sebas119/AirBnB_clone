87808eec427434e2c412770d27c885a1bece4812
Sebastian Lopez <lopez.sebastian@correounivalle.edu.co>
2020-02-13 22:16:26 +0000

    Create a BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 
+"""
+    Defines a class BaseModel.
+"""
+
 from datetime import datetime
 from uuid import uuid4
 
 
 class BaseModel:
+    """Represent a BaseModel."""
 
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel"""
+
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(
+                            val, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, val)
 
     def __str__(self):
+        """Return a string representation of the class"""
+
         return "[{:s}] ({:s}) {:s}".format(
             self.__class__.__name__,
             self.id,
@@ -19,9 +37,12 @@ class BaseModel:
         )
 
     def save(self):
+        """Update the date field"""
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Return a dictionary representation of the instance"""
 
         rep = {
             "__class__": self.__class__.__name__,
