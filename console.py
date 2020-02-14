1f173a91341d717a55a16825bda7fe3abd6b9968
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-02-14 13:46:02 +0000

    Console 0.0.1 Done

--- console.py
+++ console.py
@@ -1,7 +1,34 @@
-# /usr/bin/python3
+#!/usr/bin/python3
 
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """Hbnb command processor."""
+
     prompt = '(hbnb) '
+    file = None
+
+    def emptyline(self):
+        """Empty line method"""
+        pass
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+
+        quit()
+        return True
+
+    def help_quit(self):
+        print("Quit command to exit the program\n")
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        return True
+
+    def help_EOF(self):
+        print("EOF command to exit the program\n")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
