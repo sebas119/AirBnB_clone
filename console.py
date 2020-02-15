702138ad736475d0ebd8e73709036264fab0c074
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-02-15 16:16:30 +0000

    Show command added to print the string representation of an object

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from models.base_model import BaseModel
+from models import storage
 import cmd
 
 
@@ -45,6 +46,33 @@ class HBNBCommand(cmd.Cmd):
 
     def help_create(self):
         print("Create a BaseModel and save the json in a file\n")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance
+        based on the class name and id"""
+
+        if len(arg) > 0:
+            list_arg = arg.split()
+            objects = storage.all()
+            if len(list_arg) == 2:
+                clsId = '.'.join(list_arg)
+                if clsId in objects:
+                    print(objects[clsId])
+                else:
+                    print("** no instance found **")
+            else:
+                if len(list_arg) == 1:
+                    objs_cls = [e.split('.')[0] for e in list(objects.keys())]
+                    if arg not in objs_cls:
+                        print("** class doesn't exist **")
+                    else:
+                        print("** instance id missing **")
+        else:
+            print("** class name missing **")
+
+    def help_show(self):
+        print("Prints the string representation of an instance"
+              " based on the class name and id\n")
 
 
 if __name__ == '__main__':
