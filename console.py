9b3ab13586ab3a032d3a6113000b406d6df5d636
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-02-15 17:53:21 +0000

    All command added to print all string representation of all instances

--- console.py
+++ console.py
@@ -71,8 +71,58 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def help_show(self):
-        print("Prints the string representation of an instance"
-              " based on the class name and id\n")
+
+        msg = "Prints the string representation of an instance "
+        msg += "based on the class name and id\n"
+        print(msg)
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+
+        if len(arg) > 0:
+            list_arg = arg.split()
+            objects = storage.all()
+            if len(list_arg) == 2:
+                clsId = '.'.join(list_arg)
+                if clsId in objects:
+                    del objects[clsId]
+                    storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                if len(list_arg) == 1:
+                    objs_cls = [e.split('.')[0] for e in list(objects.keys())]
+                    if arg not in objs_cls:
+                        print("** class doesn't exist **")
+                    else:
+                        print("** instance id missing **")
+        else:
+            print("** class name missing **")
+
+    def help_destroy(self):
+        print("Deletes an instance based on the class name and id\n")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances
+        based or not on the class name"""
+
+        objects = storage.all()
+        if len(arg) > 0:
+            clsObjs = [str(v) for k, v in objects.items()
+                       if arg == k.split('.')[0]]
+            if len(clsObjs) > 0:
+                print(clsObjs)
+            else:
+                print("** class doesn't exist **")
+        else:
+            allObjs = [str(v) for k, v in objects.items()]
+            print(allObjs)
+
+    def help_all(self):
+
+        msg = "Prints all string representation of all instances "
+        msg += "based or not on the class name"
+        print(msg)
 
 
 if __name__ == '__main__':
