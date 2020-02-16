23599c03a43eb39d6a8317eeb3845bca40ceaab8
sebas119 <lopez.sebastian@correunivalle.edu.co>
2020-02-16 21:57:55 +0000

    All the models and its class attributes

--- models/state.py
+++ models/state.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""
+    Defines a class State.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a State."""
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new State"""
+
+        super().__init__(*args, **kwargs)
