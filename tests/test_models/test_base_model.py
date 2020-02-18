6827995cd88a2755acd65df30b7bf4504a608b24
Sebastian Lopez <lopez.sebastian@correounivalle.edu.co>
2020-02-18 18:40:27 +0000

    Add new test and comment the code

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
+
+"""
+    Defines a class TestBaseModel.
+"""
+
 from models.base_model import BaseModel
 import unittest
 
 
 class TestBaseModel(unittest.TestCase):
+    """Represent a TestBaseModel."""
+
+    def setUp(self):
+        """SetUp method"""
+
+        self.base_model = BaseModel()
+        self.base_model.data = "Test data"
+
+    def TearDown(self):
+        """TearDown method."""
+
+        del self.base_model
 
     def test_init_id(self):
+        """Test different id per object created"""
 
         bm1 = BaseModel()
-        bm2 = BaseModel()
-
-        self.assertNotEqual(bm1.id, bm2.id)
+        self.assertNotEqual(bm1.id, self.base_model.id)
